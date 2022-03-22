f85e5a69b0ab6df635c629ffc725b8c88f2ec6f6
CassTwoCorgs <99282985+CassTwoCorgs@users.noreply.github.com>
2022-03-22 13:43:02 -0500

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -40,6 +40,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="M" />
         <Cell value="A" />
         <Cell value="R" />
+        <Cell value="F" status="absent" />
         <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
