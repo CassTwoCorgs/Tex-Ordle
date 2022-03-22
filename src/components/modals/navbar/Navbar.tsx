e4b25a46c33a32731543e54297f68b0458031c8c
CassTwoCorgs <99282985+CassTwoCorgs@users.noreply.github.com>
2022-03-21 20:08:52 -0500

    Navbar Attempt
    Trying to get the navbar going

---
+++ src/components/modals/navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import {
+  ChartBarIcon,
+  CogIcon,
+  InformationCircleIcon,
+} from '@heroicons/react/outline'
+
+type Props = {
+  setIsInfoModalOpen: (value: boolean) => void
+  setIsStatsModalOpen: (value: boolean) => void
+  setIsSettingsModalOpen: (value: boolean) => void
+}
+
+export const Navbar = ({
+  setIsInfoModalOpen,
+  setIsStatsModalOpen,
+  setIsSettingsModalOpen,
+}: Props) => {
+  return (
+    <div className="navbar">
+      <div className="navbar-content px-5">
+        <InformationCircleIcon
+          className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
+          onClick={() => setIsInfoModalOpen(true)}
+        />
+        <div className="right-icons">
+          <ChartBarIcon
+            className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+            onClick={() => setIsStatsModalOpen(true)}
+          />
+          <CogIcon
+            className="h-6 w-6 cursor-pointer dark:stroke-white"
+            onClick={() => setIsSettingsModalOpen(true)}
+          />
+        </div>
+      </div>
+      <hr></hr>
+    </div>
+  )
+}
